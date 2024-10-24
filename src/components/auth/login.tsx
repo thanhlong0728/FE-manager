28e6b1760590e1ef385a1a21e8133c9f73f0977f
thanhlong0728 <thanhlong0728@gmail.com>
2024-10-24 16:39:32 +0700

    resend email

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,85 +1,111 @@
 "use client";
-import { Button, Col, Divider, Form, Input, Row, notification } from "antd";
+import { Button, Col, Divider, Form, Input, notification, Row } from "antd";
 import { ArrowLeftOutlined } from "@ant-design/icons";
 import Link from "next/link";
-import { signIn } from "next-auth/react";
 import { authenticate } from "@/utils/actions";
 import { useRouter } from "next/navigation";
+import ModalReactive from "./modal.reactive";
+import { useState } from "react";
 
 const Login = () => {
     const router = useRouter();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [userEmail, setUserEmail] = useState("");
+
+    const [changePassword, setChangePassword] = useState(false);
+
     const onFinish = async (values: any) => {
         const { username, password } = values;
-        // const data = await signIn("credentials", { email, password, redirect: false });
+        setUserEmail("");
+        //trigger sign-in
         const res = await authenticate(username, password);
+
         if (res?.error) {
+            //error
+            if (res?.code === 2) {
+                setIsModalOpen(true);
+                setUserEmail(username);
+                return;
+            }
             notification.error({
                 message: "Error login",
                 description: res?.error,
             });
-            if (res?.code == 2) {
-                router.push("/verify");
-            }
         } else {
+            //redirect to /dashboard
             router.push("/dashboard");
         }
     };
 
     return (
-        <Row justify={"center"} style={{ marginTop: "30px" }}>
-            <Col xs={24} md={16} lg={8}>
-                <fieldset
-                    style={{
-                        padding: "15px",
-                        margin: "5px",
-                        border: "1px solid #ccc",
-                        borderRadius: "5px",
-                    }}
-                >
-                    <legend>Đăng Nhập</legend>
-                    <Form name="basic" onFinish={onFinish} autoComplete="off" layout="vertical">
-                        <Form.Item
-                            label="Email"
-                            name="username"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "Please input your email!",
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
+        <>
+            <Row justify={"center"} style={{ marginTop: "30px" }}>
+                <Col xs={24} md={16} lg={8}>
+                    <fieldset
+                        style={{
+                            padding: "15px",
+                            margin: "5px",
+                            border: "1px solid #ccc",
+                            borderRadius: "5px",
+                        }}
+                    >
+                        <legend>Đăng Nhập</legend>
+                        <Form name="basic" onFinish={onFinish} autoComplete="off" layout="vertical">
+                            <Form.Item
+                                label="Email"
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please input your email!",
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
 
-                        <Form.Item
-                            label="Password"
-                            name="password"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "Please input your password!",
-                                },
-                            ]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please input your password!",
+                                    },
+                                ]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
 
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit">
-                                Login
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                    <Link href={"/"}>
-                        <ArrowLeftOutlined /> Quay lại trang chủ
-                    </Link>
-                    <Divider />
-                    <div style={{ textAlign: "center" }}>
-                        Chưa có tài khoản? <Link href={"/auth/register"}>Đăng ký tại đây</Link>
-                    </div>
-                </fieldset>
-            </Col>
-        </Row>
+                            <Form.Item>
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "space-between",
+                                        alignItems: "center",
+                                    }}
+                                >
+                                    <Button type="primary" htmlType="submit">
+                                        Login
+                                    </Button>
+                                    <Button type="link" onClick={() => setChangePassword(true)}>
+                                        Quên mật khẩu ?
+                                    </Button>
+                                </div>
+                            </Form.Item>
+                        </Form>
+                        <Link href={"/"}>
+                            <ArrowLeftOutlined /> Quay lại trang chủ
+                        </Link>
+                        <Divider />
+                        <div style={{ textAlign: "center" }}>
+                            Chưa có tài khoản? <Link href={"/auth/register"}>Đăng ký tại đây</Link>
+                        </div>
+                    </fieldset>
+                </Col>
+            </Row>
+            <ModalReactive isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} userEmail={userEmail} />
+        </>
     );
 };
 
